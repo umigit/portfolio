1a8a8e0a758ecf86ff6212218c1fea606beb722f
umigit <hamu56Kiki>
2019-04-07 12:09:31 +0900

    edit serializer

---
+++ myportfolio/serializer.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Work, Skill, Link
+
+class WorkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Work
+        fields = ('name', 'detail', 'url', 'github_front', 'github_back')
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = ('name', 'level')
+
+class LinkSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Link
+        fields = ('name', 'url')

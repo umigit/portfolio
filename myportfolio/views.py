3b1087a95bcad365281a76b5c79c34e13fd5bb4d
umigit <hamu56Kiki>
2019-04-07 12:17:22 +0900

    add viewsets to views.py

--- myportfolio/views.py
+++ myportfolio/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+import django_filters
+from rest_framework import viewsets, filters
+from .models import Work, Skill, Link
+from .serializer import WorkSerializer, SkillSerializer, LinkSerializer
 
-# Create your views here.
+class WorkViewSet(viewsets.ModelViewSet):
+    queryset = Work.objects.all()
+    serializer_class = WorkSerializer
+
+class SkillViewSet(viewsets.ModelViewSet):
+    queryset = Skill.objects.all()
+    serializer_class = SkillSerializer
+
+class LinkViewSet(viewsets.ModelViewSet):
+    queryset = Link.objects.all()
+    serializer_class = LinkSerializer
